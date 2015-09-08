fd56389ff0a2daed67f6eef708d857a0866116c1
Jin Li <jinlmsft@hotmail.com>
2015-09-07 22:39:41 -0700

    Add check for Internet Connection for VMHub.iOS
    Add check for Internet Connection for VMHub.iOS

--- AppSuite.iOS/VMHub.Swift/AppLibrary/AppLibrary/AppInterface.swift
+++ AppSuite.iOS/VMHub.Swift/AppLibrary/AppLibrary/AppInterface.swift
@@ -11,6 +11,59 @@
 
 import Foundation
 
+/// Service status of a certain object (gateway, provider, etc)
+internal class ServiceState
+{
+    /// Whether the service is Live at last observation
+    private var Live = false
+    /// Last time when the service state is updated
+    /// Note that with the current implementation, CACurrentMediaTime may only be the up time of the iOS device. 
+    /// It may be fine with the ServiceState check though.
+    private var Ticks = CACurrentMediaTime()
+    /// If this time has passed, rechecked the liveness of the system
+    var CheckForLivenessInMilliseconds = 30000
+    /// Initialize an instance of the ServiceState. We always initialize the Liveness to false,
+    /// and the time of check to be one day old. This will trigger rechecking of the state.
+    init( )
+    {
+        Live = false;
+        Ticks = CACurrentMediaTime() - Double(CheckForLivenessInMilliseconds) / 1000.0
+    }
+    /// Set the state of the liveness of service
+    private func SetState( bLive : Bool)
+    {
+        Live = bLive;
+        Ticks = CACurrentMediaTime()
+    }
+    
+    /// State of the service state
+    var State : Bool
+    {
+        get
+        {
+            return Live;
+        }
+        set( newState )
+        {
+            SetState( newState);
+        }
+    }
+    /// whether need to check the state
+    internal func NeedsToCheck() -> Bool
+    {
+        if (Live)
+        {
+            return false;
+        }
+        else
+        {
+            let ticksCur = CACurrentMediaTime()
+            return ((ticksCur - Ticks) * 1000.0 >= Double (CheckForLivenessInMilliseconds))
+        }
+    
+    }
+}
+
 /// Manage Http Request
 public class OneHttpWebRequest {
     public init( urlInput: NSURL?, reqCompletionHandler: NSString? -> Void )
@@ -47,6 +100,9 @@ public class OneHttpWebRequest {
 /// GatewayHttpInterface manages connection to the hub
 public class GatewayHttpInterface {
     
+    /// Page to check when validating internet connectivity
+    private static var DefaultCheckSite = "www.bing.com"
+    
     /// Default Gateway Used
     private var DefaultGateway = "vm-hub.trafficmanager.net"
     
@@ -58,6 +114,21 @@ public class GatewayHttpInterface {
     
     /// Customer Key 
     public var CustomerKey = ""
+    
+    public var ErrorMessage : String?
+    public var ExceptionMessage : String?
+    
+    private var NetworkStatus = AppLibrary.ServiceState()
+    private var GatewayStatus = AppLibrary.ServiceState()
+    private var ProviderStatus = AppLibrary.ServiceState()
+    private var InstanceStatus = AppLibrary.ServiceState()
+    
+    /// form a service URL from a site and a path
+    public func FormURL( site: String, path: String ) -> NSURL?
+    {
+        let url = NSURL(scheme: "http", host: site, path: path)
+        return url
+    }
     
     /// form a service URL from an internal URL
     public func FormURL( path: String ) -> NSURL?
@@ -82,6 +153,41 @@ public class GatewayHttpInterface {
         return "VM Hub App Library"
     }
     
+    
+    /// Check if Internet is connected. If true, the Internet connection state will be updated. Otherwise,the Internet connection state will be checked
+    /// if it hasn't been connected, and sometime has passed from the last state updated.
+    /// Return: whether the Internet is connected.
+    public func CheckInternetConnection( bForce: Bool, completionHandler: Bool -> Void )
+    {
+        if ( bForce || NetworkStatus.NeedsToCheck())
+        {
+            let url = self.FormURL(GatewayHttpInterface.DefaultCheckSite, path: "/")
+            _ = OneHttpWebRequest( urlInput: url, reqCompletionHandler: { ( resp: NSString? ) -> Void in
+                if let retResp = resp
+                {
+                    if retResp.length > 0
+                    {
+                        completionHandler(true)
+                    }
+                    else
+                    {
+                        self.ErrorMessage = String(format: "Access to site \(GatewayHttpInterface.DefaultCheckSite) return 0B")
+                        completionHandler(false)
+                    }
+                }
+                else
+                {
+                    self.ErrorMessage = String(format: "Access to site \(GatewayHttpInterface.DefaultCheckSite) fails to complete")
+                    completionHandler(false)
+                }
+            } )
+        }
+        else
+        {
+            completionHandler(NetworkStatus.State)
+        }
+    }
+
 
 }
 
